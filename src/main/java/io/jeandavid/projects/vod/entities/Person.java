25fe7a963b5c89212cb217082e94d46852b85546
Jean-David Fiquet <jdfiquet2@gmail.com>
2015-12-23 13:21:16 +0100

    Turn Serializable impl of Person into abstract
    Since equals and hashCode are tied to concrete classes, it should not be implemented in abstract ones

--- src/main/java/io/jeandavid/projects/vod/entities/Person.java
+++ src/main/java/io/jeandavid/projects/vod/entities/Person.java
@@ -57,24 +57,10 @@ public abstract class Person implements Serializable {
   }
 
   @Override
-  public int hashCode() {
-    int hash = 0;
-    hash += (id != null ? id.hashCode() : 0);
-    return hash;
-  }
+  public abstract int hashCode();
 
   @Override
-  public boolean equals(Object object) {
-    // TODO: Warning - this method won't work in the case the id fields are not set
-    if (!(object instanceof Person)) {
-      return false;
-    }
-    Person other = (Person) object;
-    if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-      return false;
-    }
-    return true;
-  }
+  public abstract boolean equals(Object object);
 
   @Override
   public String toString() {

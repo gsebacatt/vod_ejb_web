8ff38876a4be7b70a4a741263494533f505cd32d
Jean-David Fiquet <jdfiquet2@gmail.com>
2015-12-23 13:20:16 +0100

    Remove useless warnings

--- src/main/java/io/jeandavid/projects/vod/entities/Author.java
+++ src/main/java/io/jeandavid/projects/vod/entities/Author.java
@@ -25,7 +25,6 @@ public class Author extends Person implements Serializable {
 
   @Override
   public boolean equals(Object object) {
-    // TODO: Warning - this method won't work in the case the id fields are not set
     if (!(object instanceof Author)) {
       return false;
     }
--- src/main/java/io/jeandavid/projects/vod/entities/Director.java
+++ src/main/java/io/jeandavid/projects/vod/entities/Director.java
@@ -31,7 +31,6 @@ public class Director extends Person implements Serializable {
 
   @Override
   public boolean equals(Object object) {
-    // TODO: Warning - this method won't work in the case the id fields are not set
     if (!(object instanceof Director)) {
       return false;
     }
